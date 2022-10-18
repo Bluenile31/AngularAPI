8e48a13e69a7bbdb451f5f6eae7b293a0eb4830a
Bluenile31 <nileshhosure47@gmail.com>
2022-10-18 15:51:55 +0530

    commit

--- AngularAPI/Controllers/UserController.cs
+++ AngularAPI/Controllers/UserController.cs
@@ -1,8 +1,10 @@
-﻿using AngularAuthAPI.Context;
+﻿using AngularAPI.Helper;
+using AngularAuthAPI.Context;
 using AngularAuthAPI.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AngularAuthAPI.Controllers
 {
@@ -36,7 +38,27 @@ namespace AngularAuthAPI.Controllers
         {
             if (userObj == null)
                 return BadRequest();
+            //check username
+            if (await CheckUserNameExistAsync(userObj.Username))
+                return BadRequest(new
+                {
+                    Message = " Username already exists"
+                });
+
+            //check email
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exist!" });
+
+            //check password
+            var pass = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new {Message = pass.ToString() });
+
+
             //if(string.IsNullOrEmpty(userObj.Username))
+            userObj.Password= PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
             await _authContext.Users.AddAsync(userObj);
             await _authContext.SaveChangesAsync();
             return Ok(new
@@ -44,5 +66,25 @@ namespace AngularAuthAPI.Controllers
                 );
 
         }
+
+        private  Task<bool> CheckUserNameExistAsync(string username)
+            => _authContext.Users.AnyAsync(x=>x.Username == username);
+        
+        private  Task<bool> CheckEmailExistAsync(string email)
+            => _authContext.Users.AnyAsync(x => x.Email == email);
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(password.Length< 8)
+                sb.Append("Minimum password length should be 8" + Environment.NewLine);
+            /*if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("Password Should be Alphanumeric" + Environment.NewLine);
+            if (!(Regex.IsMatch(password, "[<,>,@,#, +, *, ?, ^, $, (, ), {, }, |, \\]")))
+                sb.Append("Password Should contain special character" + Environment.NewLine);*/
+            return sb.ToString();
+          
+        }
+        
     }
 }
